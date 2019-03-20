6d2230cd2aa2845cbd8146f58165df5bd7aa2b1b
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-03-19 22:59:11 -0400

    asdf

--- Results/getGTPath.m
+++ Results/getGTPath.m
@@ -5,6 +5,12 @@ function[path] = getGTPath(dsName, subType, seq)
        path = strcat('D:/DLData/Airsim/', subType, int2str(seq), '/');
     elseif strcmp(dsName,'euroc')
        path = strcat('D:/DLData/EuRoc/mh_', subType, int2str(seq), '/'); 
+    elseif strcmp(dsName, 'kitti')
+        if seq < 10
+            path = strcat('D:\DLData\KITTI\odom\dataset\sequences\0', subType, int2str(seq), '/'); 
+        else
+            path = strcat('D:\DLData\KITTI\odom\dataset\sequences\', subType, int2str(seq), '/'); 
+        end
     end
     
 
