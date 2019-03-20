0bcab39f56ac732240e9d68230e7efe663e7beb8
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-03-19 20:22:16 -0400

    aaasdf

--- main_cnn.py
+++ main_cnn.py
@@ -7,12 +7,12 @@ import numpy as np
 import time
 from git_branch_param import *
 
-dsName = 'airsim'
-subType= 'mrseg'
+dsName = 'euroc'
+subType= 'none'
 def train():
-    dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[0], isTrain=True)
+    dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[1,2,3,5], isTrain=True)
     train, val = dm.trainSet, dm.valSet
-    mc = ModelContainer_CNN(Model_CNN_0())
+    mc = ModelContainer_CNN(Model_CNN_0(dsName))
     wName = 'Weights/' + branchName() + '_' + dsName + '_' + subType
     # mc.load_weights(wName, train=True)
     mc.fit(train, val, batch_size=64, epochs=40,
@@ -25,21 +25,20 @@ def testModel():
     return mc
 
 def test():
-    for seq in range(0,3):
+    for seq in range(1,6):
         commName = 'Results/Data/' + branchName() + '_' + dsName + '_'
         commName += subType + str(seq) if dsName == 'airsim' else str(seq)
         dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[seq], isTrain=False)
         dataset = dm.testSet
 
-        for i in range(0, 1):
-            mc = testModel()
-            pr_du, pr_dw, du_cov, dw_cov, pr_dtrans, loss = mc.predict(dataset)
+        mc = testModel()
+        pr_du, pr_dw, du_cov, dw_cov, pr_dtrans, loss = mc.predict(dataset)
+        np.savetxt(commName + '_du.txt', pr_du)
+        np.savetxt(commName + '_dw.txt', pr_dw)
+        np.savetxt(commName + '_du_cov.txt', du_cov)
+        np.savetxt(commName + '_dw_cov.txt', dw_cov)
+        # np.savetxt(commName + '_dtrans.txt', pr_dtrans)
 
-            np.savetxt(commName + '_du.txt', pr_du)
-            np.savetxt(commName + '_dw.txt', pr_dw)
-            np.savetxt(commName + '_du_cov.txt', du_cov)
-            np.savetxt(commName + '_dw_cov.txt', dw_cov)
-            #np.savetxt(commName + '_dtrans.txt', pr_dtrans)
 
 if __name__ == '__main__':
     s = time.time()
