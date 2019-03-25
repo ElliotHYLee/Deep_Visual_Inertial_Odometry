b38be88045d55919e4a3f427fb78a00285dbc1e0
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-03-24 22:59:41 -0400

    asdf

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import numpy as np
 import time
 from git_branch_param import *
 
-dsName = 'euroc'
+dsName = 'kitti'
 subType= 'none'
 
 wName = 'Weights/' + branchName() + '_' + dsName + '_' + subType
 resName = 'Results/Data/' + branchName() + '_' + dsName + '_'
 
 def train():
-    dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[1, 2, 3, 5], isTrain=True)
+    dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[0,2,4,6], isTrain=True)
     train, val = dm.trainSet, dm.valSet
     mc = ModelContainer_CNN(Model_CNN_0(dsName))
     #mc.load_weights(wName, train=True)
     mc.fit(train, val, batch_size=10, epochs=40, wName=wName, checkPointFreq=1)
 
 def test():
-    for seq in range(1,6):
+    for seq in range(0,10):
         commName = resName + subType + str(seq) if dsName == 'airsim' else resName + str(seq)
         dm = VODataSetManager_CNN(dsName=dsName, subType=subType, seq=[seq], isTrain=False)
         dataset = dm.testSet
