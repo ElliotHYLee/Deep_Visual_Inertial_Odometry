6bb9e6d85fa5a756d6a717bb966082d102219500
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-04-07 16:13:37 -0400

    asf

--- Results/show_cnn.m
+++ Results/show_cnn.m
@@ -1,7 +1,7 @@
 clc, clear, close all
-dsName = 'euroc';
-subType = '';
-seq = 5;
+dsName = 'airsim';
+subType = 'mr';
+seq = 2;
 
 %% Get Ground Truth Info.
 gtPath = getGTPath(dsName,subType, seq);
