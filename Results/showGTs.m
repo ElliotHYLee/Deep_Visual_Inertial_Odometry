592ff2a81edff1dc9ee92a17d802f3f1d380d35c
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-07-14 18:39:34 -0700

    sadf

--- Results/showGTs.m
+++ Results/showGTs.m
@@ -1,6 +1,6 @@
 clc, clear, close all
-dsName = 'MyCar';
-subType = '';
+dsName = 'mycar';
+subType = 'none';
 seq = 2;
 
 %% Get Ground Truth Info.
@@ -34,13 +34,14 @@ zlabel('Position_Z', 'fontsize', 14)
 % zlim([-100 100])
 
 
+dsName = 'Custom';
 
 w = 1000;
 h = 1200;
 fig = figure('Renderer', 'painters', 'Position', [600 100 w h]);
 axes('Position', [0, 0.95, 1, 0.05] ) ;
 set( gca, 'Color', 'None', 'XColor', 'None', 'YColor', 'None' ) ;
-strTitle = strcat(dsName, {' '}, subType, {' '}, 'Sequence', {' '}, int2str(seq));
+strTitle = strcat(dsName, {' '}, 'Sequence', {' '}, int2str(seq));
 text( 0.5, 0, strTitle, 'FontSize', 16', 'FontWeight', 'Bold','HorizontalAlignment', 'Center', 'VerticalAlignment', 'Bottom' ) ;
 
 subplot(6,2,[1,3,5])
@@ -48,48 +49,56 @@ plot3(gt_pos(:,1), gt_pos(:,2), gt_pos(:,3), 'r')
 grid on
 view(45,30)
 title('Position')
-xlabel('Position_X', 'fontsize', 14)
-ylabel('Position_Y', 'fontsize', 14)
-zlabel('Position_Z', 'fontsize', 14)
+xlabel('Position_X, m', 'fontsize', 14)
+ylabel('Position_Y, m', 'fontsize', 14)
+zlabel('Position_Z, m', 'fontsize', 14)
+
 zlim([-100 100])
+
 
 subplot(6,2,2)
 plot(gt_du(:,1), 'r.')
 % ylim([-0.1, 2])
-ylabel('dU_X, m', 'fontsize', 14)
+ylabel('\DeltaU_X, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 title('Tranlational Movement')
 subplot(6,2,4)
 plot(gt_du(:,2), 'r.')
-ylabel('dU_Y, m', 'fontsize', 14)
+ylabel('\DeltaU_Y, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 subplot(6,2,6)
 plot(gt_du(:,3), 'r.')
-ylabel('dU_Z, m', 'fontsize', 14)
-
+ylabel('\DeltaU_Z, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 
 subplot(6,2,7)
 plot(gt_dw(:,1), 'r.')
-ylabel('dW_X, rad', 'fontsize', 14)
+ylabel('\DeltaW_X, rad', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 title('Angular Displacement')
 subplot(6,2,9)
 plot(gt_dw(:,2), 'r.')
-ylabel('dW_Y, rad', 'fontsize', 14)
+ylabel('\DeltaW_Y, rad', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 subplot(6,2,11)
 plot(gt_dw(:,3), 'r.')
-ylabel('dW_Z, rad', 'fontsize', 14)
-
+ylabel('\DeltaW_Z, rad', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
  
 subplot(6,2,8)
 plot(gt_dtr(:,1), 'r.')
-ylabel('dtr_X, rad', 'fontsize', 14)
+ylabel('\DeltaVu_X, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 % ylim([-0.1, 2])
 title('Overall Displacement')
 subplot(6,2,10)
 plot(gt_dtr(:,2), 'r.')
-ylabel('dtr_Y, rad', 'fontsize', 14)
+ylabel('\DeltaVu_Y, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 subplot(6,2,12)
 plot(gt_dtr(:,3), 'r.')
-ylabel('dtr_Z, rad', 'fontsize', 14)
-
+ylabel('\DeltaVu_Z, m', 'fontsize', 14)
+xlabel('Data Points', 'fontsize', 14)
 
 
 
